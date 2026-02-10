95e5f518b958049b2b495db3f75aaae714fb995d
Cowanbas <131304556+COWANBAS@users.noreply.github.com>
2026-02-10 09:35:10 -0300

    Update Anjo da Guarda.py

--- Anjo da Guarda.py
+++ Anjo da Guarda.py
@@ -1,19 +1,32 @@
 import psutil
 import time
+import keyboard  
 
 def is_tibia_running():
     return any(
-        process.info['name'] and process.info['name'].lower() == 'client' # Mude o nome para client.exe se pretende usar no Windows.
-        for process in psutil.process_iter(['name'])
-    )  
+        process.info['name']
+        and process.info['name'].lower() == 'client' # Mude o nome para "client.exe" se pretende usar no Windows.
+        for process in psutil.process_iter(['name']))
+        
+def press_shift_at():
 
-def main(): 
+# Batendo Utamo Vita
+    keyboard.press('shift')
+    keyboard.press_and_release('@')
+    keyboard.release('shift')
+
+def main():
+    tibia_aberto = False
 
     while True:
         if is_tibia_running():
-            time.sleep(1)
+            if not tibia_aberto:
+                tibia_aberto = True
         else:
-            time.sleep(1)
+            if tibia_aberto:
+                tibia_aberto = False
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
